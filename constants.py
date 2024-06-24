ded1d85799a9b6d12ed0f01cd08551bd0486b7c4
7wik <ksairam.satwik.eee14@itbhu.ac.in>
2024-06-24 13:18:22 -0700

    changed gcp creds file

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@
 File for constants
 """
 IDENTIFIERS = ["AUTOMOTIVE\n.*\n", "Pb\n.*\n", "RoHS\n.*\n","e3","HALOGEN\nFREE", "GREEN"]
-GOOGLE_APPLICATION_CREDENTIALS_PATH = "southern-waters-425700-f8-5b241da600fb.json"
+GOOGLE_APPLICATION_CREDENTIALS_PATH = "southern-waters-425700-f8-96a71a640a0c.json"
